290813483f12732a2502a2ceff46683dce5896e6
Victoria Meng <nights1ststar@protonmail.com>
2018-03-28 01:10:01 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,13 @@
 class Patient
   attr_reader :appointments
-  
+
   def initialize(name)
     @name = name
     @appointments = []
   end
 
   def add_appointment(appointment)
+    @appointments << appointment
+    
   end
 end
